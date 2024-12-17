e94c36e05dee4cde8b28fd67fc61c00e844997be
tomasgnzlz <tgonati327@g.educaand.es>
2024-12-17 10:13:19 +0100

    aaa

---
+++ pla2/src/App.js
@@ -0,0 +1,23 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import React, {Component} from 'react';
+import {Button} from 'reactstrap';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
